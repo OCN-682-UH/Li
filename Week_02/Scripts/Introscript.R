78303f53f3730f8541f5d92b2e30216a4387dcf7
Timothy Li <tli7@hawaii.edu>
2026-09-07 11:08:07 -1000

    I am adding Introscript

---
+++ Week_02/Scripts/Introscript.R
@@ -0,0 +1,23 @@
+##### This is my first script. I am learning how to import data #####
+##### Created by Timothy Li #####
+##### Created on 2026-09-07 #####
+#################################################################
+
+##### Load Libraries ##### 
+library(tidyverse)
+library(here)
+
+###### Read in my data #####
+
+weightdata <- read_csv(here("Week_02", "Data", "weightdata.csv"))
+
+##### Data Analysis #####
+
+head(weightdata)
+tail(weightdata)
+view(weightdata)
+
+
+
+
+
